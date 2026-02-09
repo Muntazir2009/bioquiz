755eb5d97fb5a2be58fd1e3fa0bc0a4fa11b5210
nicronplayz <102378365+nicronplayz@users.noreply.github.com>
2026-02-09 20:17:03 +0530

    Create presentation-data.js

---
+++ js/presentation-data.js
@@ -0,0 +1,21 @@
+const slides = [
+  {
+    title: "Cell – The Basic Unit of Life",
+    points: [
+      "All living organisms are made of cells",
+      "Cells carry out life processes",
+      "Discovered by Robert Hooke"
+    ],
+    image: "images/diagrams/cell.png"
+  },
+
+  {
+    title: "Types of Cells",
+    points: [
+      "Prokaryotic cells",
+      "Eukaryotic cells",
+      "Plant and animal cells"
+    ],
+    image: "images/diagrams/types.png"
+  }
+];
