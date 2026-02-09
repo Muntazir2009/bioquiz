7b9331a4e975f9f4ab44cb7e76e1375efe42909b
nicronplayz <102378365+nicronplayz@users.noreply.github.com>
2026-02-09 23:15:12 +0530

    Update presentation.js

--- js/presentation.js
+++ js/presentation.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".reveal-block").forEach(block => {
+  document.querySelectorAll(".reveal").forEach(block => {
     block.addEventListener("click", () => {
       block.classList.toggle("revealed");
     });
